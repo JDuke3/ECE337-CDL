7e5323730b53f9f307594b4b0ce168befb190b68
graha229 <122322182+graha229@users.noreply.github.com>
2025-05-01 11:56:29 -0400

    Update RX_edge_det.sv

--- RX USB/source/RX_edge_det.sv
+++ RX USB/source/RX_edge_det.sv
@@ -2,27 +2,24 @@
 
 module RX_edge_det (
     input clk, n_rst,
-    input logic DP_sync,
+    input logic DP_IN,
     output logic edge_flag
 );
     logic ff1, ff2;
-    logic next_edge_flag;
 
     always_ff @(posedge clk, negedge n_rst ) begin
         if(1'b0 == n_rst) begin
             ff1 <= 1'b1;
             ff2 <= 1'b1;
-            edge_flag <= 1'b0;
         end
         else begin
-            ff1 <= DP_sync;
+            ff1 <= DP_IN;
             ff2 <= ff1;
-            edge_flag <= next_edge_flag;
         end
     end
 
     always_comb begin
-        next_edge_flag = (ff1 & ~ff2);
+        edge_flag = (ff1 ^ ff2);
     end
 
 
